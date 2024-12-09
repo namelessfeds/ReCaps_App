4af13ee71ff9c3b4a58641f7c6c9503119eae3bb
Federica Bertini <federica.bertini.f@gmail.com>
2024-12-09 11:29:19 +0100

    HomepAGE

---
+++ ReCaps_App/ReCaps_App/MainPageView.swift
@@ -0,0 +1,18 @@
+//
+//  MainPageView.swift
+//  ReCaps_App
+//
+//  Created by Federica Bertini on 09/12/24.
+//
+
+import SwiftUI
+
+struct MainPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainPageView()
+}
---
+++ ReCaps_App/ReCaps_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReCaps_AppApp.swift
+//  ReCaps_App
+//
+//  Created by Federica Bertini on 09/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ReCaps_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
