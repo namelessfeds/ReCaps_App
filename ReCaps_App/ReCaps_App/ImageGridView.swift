9500c0ea71cedc99bd124231dd537c1ae53612a3
Federica Bertini <federica.bertini.f@gmail.com>
2024-12-16 12:44:21 +0100

    imagepicker for grid

--- ReCaps_App/ReCaps_App/ImageGridView.swift
+++ ReCaps_App/ReCaps_App/ImageGridView.swift
@@ -21,15 +21,31 @@ struct ImageGridView: View {
         Button(action: {
             isPickerPresented = true
         }) {
-            if let image = selectedImage {
-                Image(uiImage: image)
+            ZStack {
+                Rectangle()
+                    .frame(width: 170, height: 170)
+                    .cornerRadius(25)
+                    .foregroundStyle(Color(.systemGray6))
+                Image(systemName: "photo")
                     .resizable()
-                    .frame(width: 350, height: 170)
+                    .frame(width: 31.75, height: 25)
                     .clipShape(Rectangle())
-                    .cornerRadius(30)
+                    .cornerRadius(3.5)
+                    .foregroundColor(Color.gray)
+                    .opacity(0.9)
                     .padding(.all, 10)
-                imageFunc.addPhotosToDataItem(dataItem: dataItem!, image: selectedImage!)
-            }
+                VStack {
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        Image(systemName: "plus.circle.fill")
+                            .font(.system(size: 23))
+                    }
+                }
+                .padding()
+                    
+            }.frame(width: 170, height: 170)
+            
         }
         .sheet(isPresented: $isPickerPresented) {
             PhotoPicker(selectedImage: $selectedImage)
